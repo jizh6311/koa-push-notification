be225a4175b8a8110458430d4bcb844252b24f67
jizh6311 <zjwhut2010@hotmail.com>
2020-04-18 17:01:24 -0600

    Initial notification pushes once visit the page

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const serve = require('koa-static');
+const webpush = require("web-push");
+const path = require("path");
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
+const router = new Router();
+
+app.use(serve(path.join(__dirname, "client")));
+app.use(bodyParser());
+
+// Replace with your email
+webpush.setVapidDetails(
+  process.env.MAILTO,
+  process.env.PUBLIC_VAPID_KEY,
+  process.env.PRIVATE_VAPID_KEY
+);
+
+router.post('/subscribe', (ctx, next) => {
+  // Get pushSubscription object
+  const subscription = ctx.request.body;
+
+  // Send 201 - resource created
+  ctx.status = 201;
+  
+  // Create payload
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  // Pass object into sendNotification
+  webpush
+  .sendNotification(subscription, payload)
+  .catch(err => console.error(err));
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+  
+app.listen(5000);
